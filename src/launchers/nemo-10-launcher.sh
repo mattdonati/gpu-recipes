1a82e66ff02b42b45b9f8e1fbdafc900c3a8a7a5
mattdonati <matt.donati@gmail.com>
2025-08-17 17:10:40 +0000

    dell run yaml and launcher

--- src/launchers/nemo-10-launcher.sh
+++ src/launchers/nemo-10-launcher.sh
@@ -83,6 +83,66 @@ if [[ -n "${TOKENIZER_PATH}" ]]; then
   echo ""
 fi
 
+# export WARMUP=True
+# export DGXNGPU=8
+# export DGXSYSTEM="XE9680lx8H200-SXM-141GB_1x8x2xtp1pp1cp2"
+
+# export NCCL_MIN_P2P_NCHANNELS=32;
+# export NCCL_MIN_CTAS=32;
+# export NCCL_NCHANNELS_PER_NET_PEER=32;
+# export TP_COMM_OVERLAP=True
+# export MC_TP_OVERLAP_AG=True
+# export MC_TP_OVERLAP_RS=True
+# export MC_TP_OVERLAP_RS_DGRAD=True
+# export CUBLAS_FORCE_XMMA_KERNEL_INIT=DEVICE
+# export NVTE_RS_STRIDED_ATOMIC=2
+# export LORA_A2A=1
+
+# export POSSIBLE_USER_WARNINGS=0
+# export NVTE_FLASH_ATTN=0
+# export NVTE_FUSED_ATTN=1
+# export CUDNN_FRONTEND_ATTN_DP_WORKSPACE_LIMIT=0
+# export TORCH_NCCL_AVOID_RECORD_STREAMS=1 # Disable caching NCCL communication buffer
+# export NCCL_NVLS_ENABLE=0 # Disable NVL SHARP, which don't use
+# export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+# export FP8=True
+# export FP8_AMAX_ALGO=max
+# export FP8_REDUCE_AMAX=False
+# export FP8_AMAX_HISTORY=32
+# export HYDRA_FULL_ERROR=1
+
+# export PP=1
+# export SP=1
+
+# # other
+# export MBS=1
+# export VAL_CHECK_INTERVAL=384
+
+# # hyperparameters
+# export MAX_STEPS=896
+# export LR=0.0005
+# export MINIBS=2
+# export TP=1
+# export CP=2
+# export SP=0
+# export SKIP_EVALS=3
+# export NVTE_FLASH_ATTN=0
+# export NVTE_FUSED_ATTN=1
+# export CUDNN_FRONTEND_ATTN_DP_WORKSPACE_LIMIT=0
+# export VBOOST_VALUE=1
+# export FP8_ACTIVATION=True 
+# export FP8_DPA=0
+# export NVTE_FP8_DPA_BWD=1
+# export TP_COMM_OVERLAP=1
+# export NCCL_MIN_CTAS=32
+# export UCX_TLS=self,tcp
+
+# # system parameters
+# export DGXNNODES=1
+# export WALLTIME_RUNANDTIME=UNLIMITED
+# export WALLTIME=UNLIMITED
+
 echo "Launching Torch distributed on the node rank $JOB_COMPLETION_INDEX out of $NNODES nodes"
 
 OMP_NUM_THREADS=12 torchrun \
